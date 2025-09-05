98c2804ba0a90f6c7d439741bd771aaaa8a3351e
Fabricio <fabriciorochapalmeira@gmail.com>
2025-09-05 18:06:21 -0300

    configuração inicial

---
+++ src/main/java/com/br/motopecas/MotopecasApplication.java
@@ -0,0 +1,13 @@
+package com.br.motopecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotopecasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotopecasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/motopecas/JavaSpringMotopecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.motopecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringMotopecasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
